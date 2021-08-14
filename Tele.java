0b1bf6e626baa87c281e7b8a3855ca17089276ee
Raimundo <raimundorcd@hotmail.com>
2021-08-13 23:51:38 -0300

    commit 4

---
+++ Tele.java
@@ -0,0 +1,23 @@
+class Escola{
+
+    public void exibir(){
+    
+      System.out.println("atualizar");
+    }
+    
+    public void get(){
+    
+    }
+    
+    public int set(int n){
+    
+      return n = n+2;
+    }
+    
+    
+     public static void main(String[] args) {
+    
+         System.out.println("ola mundo");
+        
+      }
+    }
